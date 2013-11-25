591ddea1f24dc1ba98bf63e258ed4d765bc22e9a
liyazhou <liyazhou@outlook.com>
2013-11-25 22:04:42 +0800

    saddsaf

---
+++ good2/src/good2/qqq.java
@@ -0,0 +1,17 @@
+package good2;
+
+public class qqq {
+
+	public qqq() {
+		// TODO Auto-generated coasnstructor stubsdf
+		
+		safsaf
+		asf
+	}
+asfsaf
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+sdfafaf
+	}
+
+}
